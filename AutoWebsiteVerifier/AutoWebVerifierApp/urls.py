75857baa7a578c3709014aff33507f88c5113db5
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2021-09-11 17:52:09 +0530

    Delete AutoWebsiteVerifier directory

--- AutoWebsiteVerifier/AutoWebVerifierApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('VerifyWeb', views.VerifyWeb, name='VerifyWeb'),
-    path('download', views.download, name='download')
-]
--- AutoWebsiteVerifier/static/js/main.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function($){
-//    $('.Exc-Before').magnificPopup({type:'image'});
-});
